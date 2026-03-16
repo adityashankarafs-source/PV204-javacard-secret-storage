7f3bfeecbd2b87456ba302c5d2291e09c01cb569
Aditya Shankar <adityashankarafs@gmail.com>
2026-03-17 00:32:52 +0530

    Add remaining integration fixes and documentation

--- applet/src/main/java/applet/Constants.java
+++ applet/src/main/java/applet/Constants.java
@@ -1,3 +1,12 @@
+package applet;
 
+public final class Constants {
 
-yes
+    private Constants() {
+    }
+
+    public static final byte INS_STORE_SECRET = 0x10;
+    public static final byte INS_LIST_SECRETS = 0x20;
+    public static final byte INS_GET_SECRET = 0x30;
+    public static final byte INS_CHANGE_PIN = 0x40;
+}
