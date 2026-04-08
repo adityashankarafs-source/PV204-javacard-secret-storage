0965e3ab44672cb13d9e5be91fe77a4694350d7c
Aditya Shankar <adityashankarafs@gmail.com>
2026-04-08 16:08:45 +0530

    Clean repo artifacts and restore Gradle run entry point

---
+++ applet/src/main/java/main/Run.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("PV204 JavaCard Secret Storage");
+        System.out.println("Main applet class: applet.MainApplet");
+        System.out.println("Run tests with: gradlew.bat test");
+        System.out.println("Build CAP with: gradlew.bat buildJavaCard");
+    }
+}
